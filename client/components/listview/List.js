6ee1fcc72c7da4e97a582bbe138793ce1aefb9d5
Taylor Wu <Taylorwu21@gmail.com>
2016-09-24 13:18:40 -0600

    added eventdetail modal

--- client/components/listview/List.js
+++ client/components/listview/List.js
@@ -11,24 +11,59 @@ class List extends React.Component {
   componentDidMount() {
     //change navbar icon and path
     this.props.dispatch({type: 'LIST_VIEW'});
+    window.jQuery('.modal-trigger').leanModal({
+      dismissible: true,
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    });
+  }
+
+  eventDetails(id) {
+    window.jQuery(`#${id}`).openModal({
+      dismissible: true,
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    });
   }
 
   render() {
     let events = this.props.events.map(event => {
       return (
-        <Link to={`/event/${event._id}`} key={event._id} className="collection-item avatar" >
-          <li>
-            <img style={{borderRadius: '0'}} src="images/icons/soccer.png" alt="" className="circle" />
-            <span className="title">{event.title}</span>
-            <p>Location Name <br/>
-               {event.category[0]}
-            </p>
-            <span className="secondary-content">{event.startTime}</span>
-          </li>
-        </Link>
-      )
+        <div key={event._id}>
+          <a className="modal-trigger collection-item avatar"
+             onClick={ (e) => {e.preventDefault();this.eventDetails(event._id);}}
+             style={{ cursor: 'pointer' }}>
+            <li>
+              <img style={{borderRadius: '0'}} src="images/icons/soccer.png" alt="" className="circle" />
+              <span className="title">{event.title}</span>
+              <p>Location Name <br/>
+                 {event.category[0]}
+              </p>
+              <span className="secondary-content">{event.startTime}</span>
+            </li>
+          </a>
+          <div id={event._id} className="modal bottom-sheet">
+            <div className="modal-content">
+            <h2 className="center">{event.title}</h2>
+            <h3>{event.date}</h3>
+            <p>Time: {event.startTime} - {event.endTime}</p>
+            <p>LOCATION NAME HERE</p>
+            <p>{event.address}</p>
+            <p>{event.city}, {event.state}</p>
+            <br />
+            <p>Description: {event.description}</p>
+            <p>URL: <a href={event.url}>{event.url}</a></p>
+            </div>
+          </div>
+        </div>
+      );
     });
-
     return (
       <div>
         <h3>Events</h3>
@@ -36,7 +71,7 @@ class List extends React.Component {
           {events}
         </ul>
       </div>
-    )
+    );
   }
 }
 
