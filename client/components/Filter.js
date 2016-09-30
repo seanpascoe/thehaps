fb936b4c710e584d12287fe8065616f973f5e23c
spflow <sean.pascoe@gmail.com>
2016-09-30 13:58:05 -0600

    add pointer style to filter inputs

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -56,14 +56,9 @@ class Filter extends React.Component {
   }
 
   render() {
-    // old way of setting default dates
-    // let defaultStartDate;
-    // let defaultEndDate;
-    //
-    // if(this.props.filter.startDate) {
-    //   defaultStartDate = moment(this.props.filter.startDate, 'x', true).format('MMMM D, YYYY');
-    //   defaultEndDate = moment(this.props.filter.endDate, 'x', true).format('MMMM D, YYYY');
-    // }
+    let style = {
+      cursor: 'pointer'
+    };
 
     let categories = this.state.categories;
     let categorySelect = [];
@@ -83,15 +78,15 @@ class Filter extends React.Component {
         <h3>Filter</h3>
         <div className="col s12">
           <label>Start Date</label>
-          <input ref="startDate" className="datepicker dpStartDate" />
+          <input style={style} ref="startDate" className="datepicker dpStartDate" />
         </div>
         <div className="col s12">
           <label>End Date</label>
-          <input ref="endDate" className="datepicker dpEndDate" />
+          <input style={style} ref="endDate" className="datepicker dpEndDate" />
         </div>
         <div className="col s12">
           <label>Filter By Category</label>
-          <select onChange={(e) => this.catSelect(e)} value={this.props.filter.selectedCategory} className="browser-default">
+          <select style={style} onChange={(e) => this.catSelect(e)} value={this.props.filter.selectedCategory} className="browser-default">
             <option value="all">All Categories</option>
             {categorySelect}
           </select>
