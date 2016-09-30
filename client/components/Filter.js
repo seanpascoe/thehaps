7eb2b9dc415a681bf4effd2bd7832ddd80bf5342
spflow <sean.pascoe@gmail.com>
2016-09-30 11:51:35 -0600

    changed logic of default dates in filter to use pickadate, now desired functionality is working

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -12,13 +12,29 @@ class Filter extends React.Component {
 
   componentDidMount() {
     window.jQuery('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 3, // Creates a dropdown of 15 years to control year
-      closeOnSelect: true,
+      selectMonths: true,
+      selectYears: 3,
       format: 'mmmm d, yyyy',
-      onClose: this.dateSelect
+      onClose: this.dateSelect,
+      // closes datepicker on date select
+      onSet: function( arg ){
+        if ( 'select' in arg ){ //prevent closing on selecting month/year
+          this.close();
+        }
+      }
     });
+  }
 
+  componentDidUpdate(prevProps) {
+    if(!prevProps.filter.endDate) {
+      debugger;
+      let picker = window.jQuery('.dpStartDate').pickadate('picker');
+      picker.set('select', parseInt(this.props.filter.startDate));
+    }
+    if(!prevProps.filter.endDate) {
+      let picker = window.jQuery('.dpEndDate').pickadate('picker');
+      picker.set('select', parseInt(this.props.filter.endDate));
+    }
   }
 
   capitalizeCat(cat) {
@@ -40,13 +56,14 @@ class Filter extends React.Component {
   }
 
   render() {
-    let defaultStartDate;
-    let defaultEndDate;
-
-    if(this.props.filter.startDate) {
-      defaultStartDate = moment(this.props.filter.startDate, 'x', true).format('MMMM D, YYYY');
-      defaultEndDate = moment(this.props.filter.endDate, 'x', true).format('MMMM D, YYYY');
-    }
+    // old way of setting default dates
+    // let defaultStartDate;
+    // let defaultEndDate;
+    //
+    // if(this.props.filter.startDate) {
+    //   defaultStartDate = moment(this.props.filter.startDate, 'x', true).format('MMMM D, YYYY');
+    //   defaultEndDate = moment(this.props.filter.endDate, 'x', true).format('MMMM D, YYYY');
+    // }
 
     let categories = this.state.categories;
     let categorySelect = [];
@@ -65,19 +82,19 @@ class Filter extends React.Component {
       <div id="slide-out1" className="side-nav">
         <h3>Filter</h3>
         <div className="col s12">
+          <label>Start Date</label>
+          <input ref="startDate" className="datepicker dpStartDate" />
+        </div>
+        <div className="col s12">
+          <label>End Date</label>
+          <input ref="endDate" className="datepicker dpEndDate" />
+        </div>
+        <div className="col s12">
           <label>Filter By Category</label>
           <select onChange={(e) => this.catSelect(e)} value={this.props.filter.selectedCategory} className="browser-default">
             <option value="all">All Categories</option>
             {categorySelect}
           </select>
-        </div>
-        <div className="col s12">
-          <label>Start Date</label>
-          <input ref="startDate" className="datepicker" value={defaultStartDate ? defaultStartDate : ''} />
-        </div>
-        <div className="col s12">
-          <label>End Date</label>
-          <input ref="endDate" className="datepicker" value={defaultEndDate ? defaultEndDate : ''}/>
         </div>
       </div>
     );
