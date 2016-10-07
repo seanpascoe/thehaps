4b398a0642d8a200e28ad39d0b3894069cb8b599
Taylor Wu <Taylorwu21@gmail.com>
2016-10-06 23:46:53 -0600

    created 404 page

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import FaFrown from 'react-icons/lib/fa/frown-o';
 
-const NotFound = () => (
-  <div>
-    <h3 className="center">404 Page Not Found</h3>
+const NotFound = () => {
+  let styles = {
+    title: {fontWeight: '300', fontSize: '30px'}
+  };
+  return(
+  <div className='center'>
+    <div style={styles.title}>OH SHIT! 404 PAGE</div>
+    <div><a href='/' style={styles.title}>Come Back Home</a></div>
+    <FaFrown style={{color: '#2C3E50'}} size={'300px'} />
   </div>
-);
+  );
+};
 
 export default NotFound;
