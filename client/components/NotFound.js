70566e6072d97c9f47dab1efdef917589a904de8
spflow <sean.pascoe@gmail.com>
2016-12-07 11:51:07 -0700

    changed notfound verbiage

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   };
   return(
   <div className='center'>
-    <div style={styles.title}>OH SHIT! 404 PAGE</div>
+    <div style={styles.title}>OH SHEET! 404 PAGE</div>
     <div><a href='/' style={styles.title}>Come Back Home</a></div>
     <FaFrown style={{color: '#2C3E50'}} size={'300px'} />
   </div>
